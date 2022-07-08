6a1372281fc3f2b4342144d598bff51413f17c7c
Hiroshi Nobuoka <hiroshi.nobuoka@gmail.com>
2022-07-07 23:14:46 -0400

    transitions refactor;

--- src/js/page-open-handler.js
+++ src/js/page-open-handler.js
@@ -1,32 +1,70 @@
 const pages = Array.from(document.querySelectorAll('.page'));
+const pageLabels = Array.from(document.querySelectorAll('.page-label'))
 const logo = document.querySelector('.logo');
 const closeBtn = document.querySelector('.close');
 
-pages.forEach(page => {
-  page.addEventListener('click', (e) => pageClickHandler(e.target));
+pageLabels.forEach((label, index) => {
+  label.addEventListener('click', (e) => pageClickHandler(index));
 })
 closeBtn.addEventListener('click', () => closeClickHandler());
 
 
 
-function pageClickHandler(activePage) {
-  pages.forEach(page => removeClass(page, 'active'));
+function pageClickHandler(activeIndex) {
+  const activePage = pages[activeIndex];
+  const inactives = pages.filter((page, index) => index !== activeIndex);
+
+  switch (activeIndex) {
+    case 0:
+      addClass(inactives[0], ['fade-right', 'inactive']);
+      addClass(inactives[1], ['fade-down', 'inactive']);
+      addClass(inactives[2], ['fade-right', 'fade-down', 'inactive']);
+      break;
+    case 1:
+      addClass(inactives[0], ['fade-left', 'inactive']);
+      addClass(inactives[2], ['fade-down', 'inactive']);
+      addClass(inactives[1], ['fade-left', 'fade-down', 'inactive']);
+    break;
+    case 2:
+      addClass(inactives[0], ['fade-up', 'inactive']);
+      addClass(inactives[2], ['fade-right', 'inactive']);
+      addClass(inactives[1], ['fade-up', 'fade-right', 'inactive']);
+    break;
+    case 3:
+      addClass(inactives[1], ['fade-up', 'inactive']);
+      addClass(inactives[2], ['fade-left', 'inactive']);
+      addClass(inactives[0], ['fade-up', 'fade-left', 'inactive']);
+    break;
+  }
+
   addClass(activePage, 'active');
   addClass(logo, 'page-view');
   removeClass(closeBtn, 'hidden');
 }
 
 function closeClickHandler() {
-  pages.forEach(page => removeClass(page, 'active'));
+  const classes = ['active', 'inactive', 'fade-up', 'fade-down', 'fade-left', 'fade-right'];
+  pages.forEach(page => {
+    removeClass(page, classes);
+  });
+
   addClass(closeBtn, 'hidden');
   removeClass(logo, 'page-view');
 }
 
 
 function addClass(element, className) {
-  element.classList.add(className);
+  if (typeof className === 'string') {
+    element.classList.add(className);
+  } else {
+    element.classList.add(...className);
+  }
 }
 
 function removeClass(element, className) {
-  element.classList.remove(className);
+  if (typeof className === 'string') {
+    element.classList.remove(className);
+  } else {
+    element.classList.remove(...className);
+  }
 }
